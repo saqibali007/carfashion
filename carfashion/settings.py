c97faee8e17a7d150ca25c2d494c6856c56879ae
Saqib Ali <saqibali007@gmail.com>
2026-05-02 00:42:28 +0530

    allow all hosts

--- carfashion/settings.py
+++ carfashion/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-carfashion-xyz123')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
